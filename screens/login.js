bf941447b2536206765b00bbafb55831fafbcc7c
bgg274 <bgg274@myy.haaga-helia.fi>
2022-05-10 16:19:49 +0300

    init project

--- screens/login.js
+++ screens/login.js
@@ -1,28 +1,15 @@
-import React, {useEffect} from 'react';
+import React from 'react';
 import { Image, Text, StyleSheet, View, ScrollView, TouchableOpacity, TextInput, Button, Alert } from 'react-native';
 import { BlurView } from 'expo-blur';
 
 import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth';
 import { initializeApp } from 'firebase/app';
-import { firebaseConfig } from './firebase-config';
-
-import { NavigationContainer, useNavigation } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-
-
-
+import { firebaseConfig } from '../firebase-config';
+import { useNavigation } from '@react-navigation/native';
 const uri = 'https://ak.picdn.net/shutterstock/videos/1060308725/thumb/1.jpg'
 const profilePicture = 'https://randomuser.me/api/portraits/men/34.jpg'
 
-function HomeScreen() {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Home Screen</Text>
-    </View>
-  );
-}
-
-  function LoginScreen() {
+export default function LoginScreen() {
 
     const [email, setEmail] = React.useState('')
     const [password, setPassword] = React.useState('')
@@ -94,70 +81,3 @@ function HomeScreen() {
       </View>
     );
   }
-
-  const Stack = createNativeStackNavigator();
-  
-  export default function App() {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Login">
-        <Stack.Screen name="Login" component={LoginScreen} />
-        <Stack.Screen name="Home" component={HomeScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
-}
-
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  image: {
-    width: '100%',
-    height: '100%',
-    resizeMode: 'cover',
-  },
-  login: {
-    width: 350,
-    height: 500,
-    borderColor: '#fff',
-    borderWidth: 2,
-    borderRadius: 10,
-    padding: 10,
-    alignItems: 'center',
-  },
-  profilePicture: {
-    width: 100,
-    height: 100,
-    borderRadius: 50,
-    borderColor: '#fff',
-    borderWidth: 1,
-    marginVertical: 30
-  },
-  input: {
-    width: 250,
-    height: 40,
-    borderColor: '#fff',
-    borderWidth: 2,
-    borderRadius: 10,
-    padding: 10,
-    marginVertical: 10,
-    backgroundColor: '#ffffff90',
-    marginBottom: 20
-  },
-  button: {
-    width: 250,
-    height: 40,
-    borderRadius: 10,
-    alignItems: 'center',
-    justifyContent: 'center',
-    marginVertical: 10,
-    borderColor: '#fff',
-    borderWidth: 1,
-  }
-
-});
